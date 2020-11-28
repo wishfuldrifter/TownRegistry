919e12d7ef886cb227d76ad04880f7305a230456
Derek Rucker <derek.rucker@gmail.com>
2020-11-28 16:28:02 -0500

    Initial commit - town registry app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TownReg.Models;
+
+namespace TownReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        public IActionResult ViewDatabase()
+        {
+            return View("Registry");
+        }
+    }
+}
